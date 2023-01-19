0881f4f51559acae7e1a0007861c2a92593c50e2
takaya <takaya39899805@gmail.com>
2023-01-19 20:16:27 +0900

    add urql

--- frontend/app/pages/_app.tsx
+++ frontend/app/pages/_app.tsx
@@ -2,11 +2,14 @@ import 'styles/globals.scss'
 import type { AppProps } from 'next/app'
 import { Provider } from 'urql'
 import { gqlClient } from 'src/libs/urql/requests'
+import { MantineProvider } from '@mantine/core'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider value={gqlClient}>
-      <Component {...pageProps} />
+      <MantineProvider withGlobalStyles withNormalizeCSS>
+        <Component {...pageProps} />
+      </MantineProvider>
     </Provider>
   )
 }
