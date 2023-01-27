d5c6b86f76ef235ac5422d62d485cf8c338d39ff
takaya <takaya39899805@gmail.com>
2023-01-27 23:13:05 +0900

    Revert "Revert "update file name""
    This reverts commit b4904652d9653df12a9fd4d0604a6b65f4c79ba6.

--- frontend/app/next.config.js
+++ frontend/app/next.config.js
@@ -1,16 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpackDevMiddleware: config => {
+  webpackDevMiddleware: (config) => {
     config.watchOptions = {
       poll: 1000,
       aggregateTimeout: 300,
     }
     return config
   },
+  pageExtensions: ['page.tsx'],
   reactStrictMode: true,
   env: {
-    GRAPHQL_ENDPOINT: 'http://localhost:8080/v1/graphql'
-  }
+    GRAPHQL_ENDPOINT: 'http://localhost:8080/v1/graphql',
+  },
 }
 
 module.exports = nextConfig
