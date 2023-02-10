5c138fdeb95bdeb61a0db050257fb4f599367c26
takaya <takaya39899805@gmail.com>
2023-02-11 03:55:54 +0900

    add article no image link

--- frontend/app/pages/components/Article.tsx
+++ frontend/app/pages/components/Article.tsx
@@ -56,8 +56,8 @@ export const Article = () => {
                     />
                   ) : (
                     <Image
-                      src='https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80'
-                      alt='Norway'
+                      src='https://www.freeiconspng.com/uploads/no-image-icon-23.jpg'
+                      alt='no image'
                       width={300}
                       height={160}
                     />
