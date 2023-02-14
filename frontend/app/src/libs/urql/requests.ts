429bbdaa63fcb0d15862df2e4d6085e55719a258
takaya <takaya39899805@gmail.com>
2023-02-15 00:21:00 +0900

    feat env file for dev and product

--- frontend/app/src/libs/urql/requests.ts
+++ frontend/app/src/libs/urql/requests.ts
@@ -1,6 +1,6 @@
 import { cacheExchange, createClient, dedupExchange, fetchExchange } from 'urql'
 
-export const GQL_ENDPOINT = process.env.GRAPHQL_ENDPOINT!
+export const GQL_ENDPOINT = process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT!
 
 export const gqlClient = createClient({
   url: GQL_ENDPOINT,
