c6ceabee04324e7a74a28cb845fd19d007b48935
takaya <takaya39899805@gmail.com>
2023-02-16 01:38:13 +0900

    add request headers to hasura

--- frontend/app/src/libs/urql/requests.ts
+++ frontend/app/src/libs/urql/requests.ts
@@ -4,5 +4,11 @@ export const GQL_ENDPOINT = process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT!
 
 export const gqlClient = createClient({
   url: GQL_ENDPOINT,
+  fetchOptions: {
+    headers: {
+      'content-type': 'application/json',
+      'x-hasura-admin-secret': process.env.NEXT_PUBLIC_ADMIN_SECRET_KEY!
+    },
+  },
   exchanges: [dedupExchange, cacheExchange, fetchExchange],
 })
