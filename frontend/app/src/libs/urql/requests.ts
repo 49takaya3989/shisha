295a948aec75417b9662f9dd7ddc4061b801d810
takaya <takaya39899805@gmail.com>
2023-02-16 22:29:18 +0900

    fix hasura secret key

--- frontend/app/src/libs/urql/requests.ts
+++ frontend/app/src/libs/urql/requests.ts
@@ -1,13 +1,14 @@
 import { cacheExchange, createClient, dedupExchange, fetchExchange } from 'urql'
 
 export const GQL_ENDPOINT = process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT!
+export const GQL_HASURA_SECRET_KEY = process.env.NEXT_PUBLIC_HASURA_SECRET_KEY!
 
 export const gqlClient = createClient({
   url: GQL_ENDPOINT,
   fetchOptions: {
     headers: {
       'content-type': 'application/json',
-      'x-hasura-admin-secret': process.env.NEXT_PUBLIC_HASURA_SECRET_KEY!,
+      'x-hasura-admin-secret': GQL_HASURA_SECRET_KEY,
     },
   },
   exchanges: [dedupExchange, cacheExchange, fetchExchange],
