5663ae3b5d125cb0f8132e48deb2126ee1067104
takaya <takaya39899805@gmail.com>
2023-02-23 20:16:09 +0900

    hide basic auth

--- frontend/app/middleware.api.ts
+++ frontend/app/middleware.api.ts
@@ -1,46 +1,53 @@
+import { withClerkMiddleware, getAuth } from '@clerk/nextjs/server'
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-import { withClerkMiddleware, getAuth } from '@clerk/nextjs/server'
 
 // Set the paths that don't require the user to be signed in
 export const privatePaths = ['/admin*', '/sign-up*']
-export const basicPaths = ['/sign-in*']
 export const isPrivate = (path: string) => {
   return privatePaths.find((x) =>
-    path.match(new RegExp(`^${x}$`.replace('*$', '($|/)')))
-  )
-}
-export const isBasic = (path: string) => {
-  return basicPaths.find((x) =>
-    path.match(new RegExp(`^${x}$`.replace('*$', '($|/)')))
+  path.match(new RegExp(`^${x}$`.replace('*$', '($|/)')))
   )
 }
 
-export const config = { matcher: '/((?!.*\\.).*)' }
+// ▼▼▼ clerkのリダイレクトと相性が悪いので一旦コメントアウト ▼▼▼
+//
+// export const basicPaths = ['/sign-in*']
+// export const isBasic = (path: string) => {
+//   return basicPaths.find((x) =>
+//     path.match(new RegExp(`^${x}$`.replace('*$', '($|/)')))
+//   )
+// }
+//
+// ▲▲▲ clerkのリダイレクトと相性が悪いので一旦コメントアウト ▲▲▲
+
+// ▼▼▼ clerkのリダイレクトと相性が悪いので一旦コメントアウト ▼▼▼
+//
+// if (isBasic(req.nextUrl.pathname)) {
+//   const authorizationHeader = req.headers.get('authorization')
+
+//   if (authorizationHeader) {
+//     const basicAuth = authorizationHeader.split(' ')[1]
+//     const [user, password] = atob(basicAuth).split(':')
+
+//     if (
+//       user === process.env.NEXT_PUBLIC_BASIC_AUTH_USER &&
+//       password === process.env.NEXT_PUBLIC_BASIC_AUTH_PWD
+//     ) {
+//       return NextResponse.next()
+//     }
+//   }
+
+//   const url = req.nextUrl
+//   url.pathname = '/api/basic'
+
+//   return NextResponse.rewrite(url)
+// }
+//
+// ▲▲▲ clerkのリダイレクトと相性が悪いので一旦コメントアウト ▲▲▲
 
 export default withClerkMiddleware((req: NextRequest) => {
-  if (isBasic(req.nextUrl.pathname)) {
-    const authorizationHeader = req.headers.get('authorization')
-
-    if (authorizationHeader) {
-      const basicAuth = authorizationHeader.split(' ')[1]
-      const [user, password] = atob(basicAuth).split(':')
-
-      if (
-        user === process.env.NEXT_PUBLIC_BASIC_AUTH_USER &&
-        password === process.env.NEXT_PUBLIC_BASIC_AUTH_PWD
-      ) {
-        return NextResponse.next()
-      }
-    }
-
-    const url = req.nextUrl
-    url.pathname = '/api/basic'
-
-    return NextResponse.rewrite(url)
-  }
-
   if (isPrivate(req.nextUrl.pathname)) {
     // if the user is not signed in redirect them to the sign in page.
     const { userId } = getAuth(req)
@@ -54,3 +61,5 @@ export default withClerkMiddleware((req: NextRequest) => {
 
   return NextResponse.next()
 })
+
+export const config = { matcher:  '/((?!_next/image|_next/static|favicon.ico).*)',};
