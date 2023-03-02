2864fd5a993200fc818ec181f3ccef8927d8592e
takaya <takaya39899805@gmail.com>
2023-03-02 23:20:44 +0900

    fix sign in components

--- frontend/app/pages/sign-in.page.tsx
+++ frontend/app/pages/sign-in.page.tsx
@@ -5,7 +5,11 @@ import { ROUTE } from 'helper/constant/route'
 const SignInPage = () => {
   return (
     <div className="flex h-screen items-center justify-center">
-      <SignIn path="/sign-in" redirectUrl={ROUTE.ADMIN_DASHBOARD} />
+      <SignIn
+        path={ROUTE.SIGN_IN}
+        redirectUrl={ROUTE.HOME}
+        signUpUrl={ROUTE.SIGN_UP}
+      />
     </div>
   )
 }
